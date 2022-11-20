cae661f78e6c9309ba86f1e6acf90516f37d1d0c
codedreamer1 <cdreamer06@gmail.com>
2022-11-20 22:14:04 +0700

    added the admin panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import './App.css';
-import Navbar from './components/misc/Navbar';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import ConnectWalletModal from './kda-wallet/components/ConnectWalletModal';
-import { txToastManager, messageToastManager, walletConnectedToastManager } from './components/tx_and_toasts/TxToastManager';
-import KeySensor from './components/hotkey_button/KeySensor';
-import Home from './pages/home';
-
+import "./App.css";
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import Navbar from "./components/misc/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ConnectWalletModal from "./kda-wallet/components/ConnectWalletModal";
+import {
+  txToastManager,
+  messageToastManager,
+  walletConnectedToastManager,
+} from "./components/tx_and_toasts/TxToastManager";
+import KeySensor from "./components/hotkey_button/KeySensor";
+import Home from "./pages/home";
+import Admin from "./pages/admin";
 
 export default function App() {
-  
   return (
-    <div className="w-full flex flex-col items-center px-12">
-      
+    <BrowserRouter>
+      <div className="w-full flex flex-col items-center px-12">
         <ToastContainer
           position="bottom-right"
           autoClose={5000}
@@ -24,16 +28,20 @@ export default function App() {
           draggable
           pauseOnHover
         />
-        <KeySensor/>
-        <ConnectWalletModal 
+        <KeySensor />
+        <ConnectWalletModal
           onNewTransaction={txToastManager}
           onNewMessage={messageToastManager}
           onWalletConnected={walletConnectedToastManager}
           buttonStyle="`border-white border-2 rounded-md h-auto px-10 py-2 hover:border-purple-300 active:border-purple-700 focus:border-purple-500 transition duration-150 ease-out"
         />
         <Navbar />
-        <Home />
-      
-    </div>
-  )
+        <Routes>
+          <Route exact path="/home" element={<Home />} key="home" />;
+          <Route exact path="/admin" element={<Admin />} key="admin" />;
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
