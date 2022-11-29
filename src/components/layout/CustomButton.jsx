7bb1d3415279f20b8b40ad233ff30cc7f266560f
ssrockstar-dev <cdreamer06@gmail.com>
2022-11-29 10:51:24 +0900

    updated the ui

--- src/components/layout/CustomButton.jsx
+++ src/components/layout/CustomButton.jsx
@@ -1,7 +1,7 @@
 function CustomButton(props) {
   return (
     <button
-      className={`border-white border-2 rounded-md h-auto px-2 sm:px-10 py-2 hover:border-purple-300 active:border-purple-700 focus:border-purple-500 transition duration-150 ease-out ${props.className}`}
+      className={`border-white border-2 rounded-md h-auto px-5 py-2 hover:border-purple-300 active:border-purple-700 focus:border-purple-500 transition duration-150 ease-out ${props.className}`}
       onClick={props.onClick ? props.onClick : () => { }}>
       {props.text}
     </button>
